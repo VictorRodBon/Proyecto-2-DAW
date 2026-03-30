5a204618c642837f84c574f5e3778c4abd6338d8
Victor Rodriguez <victorrodbon@gmail.com>
2026-03-30 15:16:34 +0200

    Modificación backend para que muestere la información correcta en el primer endpoint (enpoint vacio)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { OpinionesModule } from './opiniones/opiniones.module';
 import { LecturasModule } from './lecturas/lecturas.module';
@@ -13,5 +15,7 @@ import { UsuariosModule } from './usuarios/usuarios.module';
     LecturasModule,
     UsuariosModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
